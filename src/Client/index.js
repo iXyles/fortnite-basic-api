718897d1e3555b6681ba85a66e36c23fe0c9b466
iXyles <marcus@strandner.se>
2019-10-13 21:49:09 +0200

    Missed a 0 -.-

--- src/Client/index.js
+++ src/Client/index.js
@@ -11,7 +11,7 @@ module.exports = class Client {
     this.password = args.password || undefined;
     this.launcherToken = args.launcherToken || 'MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y=';
     this.fortniteToken = args.fortniteToken || 'ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=';
-    this.seasonStartTime = args.seasonStartTime || '157099000'; // S11 EPOCH
+    this.seasonStartTime = args.seasonStartTime || '1570990000'; // S11 EPOCH
     if (!this.email || !this.password || !this.launcherToken || !this.fortniteToken) {
       throw new Error('Constructor data was incorrect [email, password, launcherToken, fortniteToken] check docs.');
     }
